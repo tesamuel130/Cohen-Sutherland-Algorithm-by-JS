46fb650522b80beb59d3a2791ffcf2bc41016095
tesamuel130 <154377653+tesamuel130@users.noreply.github.com>
2024-11-08 01:37:51 -0900

    working on the algorithm

--- CohenSutherlandAlgorithm.js
+++ CohenSutherlandAlgorithm.js
@@ -1,8 +1,8 @@
 // Define the clipping window boundaries
-const xmin = 10,
-  ymin = 10,
-  xmax = 30,
-  ymax = 20;
+const xMin = 10,
+  yMin = 10,
+  xMax = 30,
+  yMax = 20;
 
 // Define region codes
 const INSIDE = 0; // 0000
@@ -12,53 +12,53 @@ const BOTTOM = 4; // 0100
 const TOP = 8; // 1000
 
 // Function to compute the region code for a point (x, y)
-function computeOutcode(x, y) {
+function computeOutCode(x, y) {
   let code = INSIDE;
-  if (x < xmin) code |= LEFT;
-  else if (x > xmax) code |= RIGHT;
-  if (y < ymin) code |= BOTTOM;
-  else if (y > ymax) code |= TOP;
+  if (x < xMin) code |= LEFT;
+  else if (x > xMax) code |= RIGHT;
+  if (y < yMin) code |= BOTTOM;
+  else if (y > yMax) code |= TOP;
   return code;
 }
 
 // Function implementing the Cohen-Sutherland algorithm
 function cohenSutherlandClip(x1, y1, x2, y2) {
-  let outcode1 = computeOutcode(x1, y1);
-  let outcode2 = computeOutcode(x2, y2);
+  let outCode1 = computeOutCode(x1, y1);
+  let outCode2 = computeOutCode(x2, y2);
   let accept = false;
 
   while (true) {
-    if (!(outcode1 | outcode2)) {
+    if (!(outCode1 | outCode2)) {
       accept = true;
       break;
-    } else if (outcode1 & outcode2) {
+    } else if (outCode1 & outCode2) {
       break;
     } else {
       let x, y;
-      const outcodeOut = outcode1 ? outcode1 : outcode2;
+      const outCodeOut = outCode1 ? outCode1 : outCode2;
 
-      if (outcodeOut & TOP) {
-        x = x1 + ((x2 - x1) * (ymax - y1)) / (y2 - y1);
-        y = ymax;
-      } else if (outcodeOut & BOTTOM) {
-        x = x1 + ((x2 - x1) * (ymin - y1)) / (y2 - y1);
-        y = ymin;
-      } else if (outcodeOut & RIGHT) {
-        y = y1 + ((y2 - y1) * (xmax - x1)) / (x2 - x1);
-        x = xmax;
-      } else if (outcodeOut & LEFT) {
-        y = y1 + ((y2 - y1) * (xmin - x1)) / (x2 - x1);
-        x = xmin;
+      if (outCodeOut & TOP) {
+        x = x1 + ((x2 - x1) * (yMax - y1)) / (y2 - y1);
+        y = yMax;
+      } else if (outCodeOut & BOTTOM) {
+        x = x1 + ((x2 - x1) * (yMin - y1)) / (y2 - y1);
+        y = yMin;
+      } else if (outCodeOut & RIGHT) {
+        y = y1 + ((y2 - y1) * (xMax - x1)) / (x2 - x1);
+        x = xMax;
+      } else if (outCodeOut & LEFT) {
+        y = y1 + ((y2 - y1) * (xMin - x1)) / (x2 - x1);
+        x = xMin;
       }
 
-      if (outcodeOut === outcode1) {
+      if (outCodeOut === outCode1) {
         x1 = x;
         y1 = y;
-        outcode1 = computeOutcode(x1, y1);
+        outCode1 = computeOutCode(x1, y1);
       } else {
         x2 = x;
         y2 = y;
-        outcode2 = computeOutcode(x2, y2);
+        outCode2 = computeOutCode(x2, y2);
       }
     }
   }
